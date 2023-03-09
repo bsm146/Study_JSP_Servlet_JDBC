4a85f398968f0e80ec720bee63c60b2f27f72003
BaeSeMin <bsm14666@gmail.com>
2023-03-09 16:02:44 +0900

    코테 원본

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,39 +4,61 @@ public class Test {
 
     public static void main(String[] args) {
 
+        int answer = 0;
+//        int[] number = {-2, 3, 0, 2, -5};
+        int[] number = {-3, -2, -1, 0, 1, 2, 3};
+        int numberLength = number.length;
 
-//        Statement stmt = null;
-//        ResultSet rs = null;
-//        Connection con = null;
-//
-//        try {
-//            String url = "jdbc:mysql://localhost:3306/board";
-//            String user = "root";
-//            String password = "1234";
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            con = DriverManager.getConnection(url, user, password);
-//            System.out.println("DB 연결 성공");
-//        }
-//        catch (Exception e) {
-//            System.out.println("DB 연결 실패");
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            String sql = "SELECT * FROM board";
-//            stmt = con.createStatement();
-//            rs = stmt.executeQuery(sql);
-//
-//            while (rs.next()) {
-//                System.out.println(rs.getString(1) + "   " + rs.getString(2));
-//            }
-//
-//
-//        }
-//        catch(SQLException ex) {
-//            System.out.print("조회 실패");
-//            System.out.print("SQLException : " + ex.getMessage());
-//            ex.printStackTrace();
-//        }
+        int a = 0;
+        int b = 1;
+        int c = 2;
+
+        while (true) {
+
+            if (number[a] + number[b] + number[c] == 0) {
+                answer++;
+                if (c != numberLength - 1) {
+                    c++;
+                    continue;
+                } else {
+                    if (b != numberLength - 2) {
+                        b++;
+                        c = b + 1;
+                        continue;
+                    } else {
+                        if (a != numberLength - 3) {
+                            a++;
+                            b = a + 1;
+                            c = a + 2;
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            } else {
+                if (c != numberLength - 1) {
+                    c++;
+                    continue;
+                } else {
+                    if (b != numberLength - 2) {
+                        b++;
+                        c = b + 1;
+                        continue;
+                    } else {
+                        if (a != numberLength - 3) {
+                            a++;
+                            b = a + 1;
+                            c = a + 2;
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
     }
 }
